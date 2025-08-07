f89c6e1a076ac3e27cc2905b6aaf0fa86a585cf8
DamianoMura <mura.damiano14@gmail.com>
2025-08-07 11:35:22 +0200

    [del] page creator.js

--- middlewares/pageCreator.js
+++
@@ -1,8 +0,0 @@
-//defining the pageCreator Middleware
-const pageCreator = ( req, res, next ) => {
- console.log("viene usato il page creator")
- next();
-}
-
-//export middleware
-module.exports=pageCreator;
